328cc9f725d0e204c55705bdbecd481b5d99e5f4
Jeremiah Morris <hopper@whpress.com>
2009-11-29 17:57:31 +0000

    Custom Mac framework dependencies
    git-svn-id: svn://svn.code.sf.net/p/marathon/code/frameworks/trunk@4050 a75d40c9-0969-4759-85e5-97cb871b7ed4

---
+++ mad/build_config.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -o errexit
+
+## general compilation targets
+arches=( i386 ppc )
+sdk="/Developer/SDKs/MacOSX10.4u.sdk"
+minver="10.4"
+gccver="4.0"
+
+## project-specific options
+proj="mad"
+config_h_filename="config.h"
+config_h_dir="."
+extra_config_opts=""
+
+### end of config
+
+
+# make clean copy of config.h directory
+configdir="config"
+if [ -e $configdir ]; then
+  rm -rf $configdir
+fi
+mkdir $configdir
+outconfig="$configdir/$config_h_filename"
+touch $outconfig
+
+# make clean copy of source tree
+libdir="lib$proj"
+tempdir="temp_$libdir"
+if [ -e $tempdir ]; then
+  rm -rf $tempdir
+fi
+cp -a $libdir $tempdir
+inconfig="$tempdir/$config_h_dir/$config_h_filename"
+
+# configure for each arch
+export CC=gcc-$gccver
+export CXX=g++-$gccver
+export LD=g++-$gccver
+for arch in "${arches[@]}"; do
+  export CFLAGS="-arch $arch -isysroot $sdk -mmacosx-version-min=$minver"
+  export CXXFLAGS="$CFLAGS"
+  export LDFLAGS="$CFLAGS"
+  export OBJCFLAGS="$CFLAGS"
+  
+  # all set up, now run
+  pushd $tempdir
+  ./configure $extra_config_opts
+  popd
+  
+  # add this config.h to the master copy
+  fpm=DEFAULT
+  if [ "$arch" == "i386" ]; then
+    fpm=INTEL
+  fi
+  if [ "$arch" == "ppc" ]; then
+    fpm=PPC
+  fi
+  
+  cat >> $outconfig <<EOM
+#ifdef __${arch}__
+#define FPM_$fpm
+EOM
+  cat $inconfig >> $outconfig
+  cat >> $outconfig <<EOM
+#endif
+EOM
+
+done
+
+# remove source tree copy
+rm -rf $tempdir
---
+++ sndfile/build_config.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -o errexit
+
+## general compilation targets
+arches=( i386 ppc )
+sdk="/Developer/SDKs/MacOSX10.4u.sdk"
+minver="10.4"
+gccver="4.0"
+
+## project-specific options
+proj="sndfile"
+config_h_filename="config.h"
+config_h_dir="src"
+extra_config_opts=""
+
+### end of config
+
+
+# make clean copy of config.h directory
+configdir="config"
+if [ -e $configdir ]; then
+  rm -rf $configdir
+fi
+mkdir $configdir
+outconfig="$configdir/$config_h_filename"
+touch $outconfig
+
+# make clean copy of source tree
+libdir="lib$proj"
+tempdir="temp_$libdir"
+if [ -e $tempdir ]; then
+  rm -rf $tempdir
+fi
+cp -a $libdir $tempdir
+inconfig="$tempdir/$config_h_dir/$config_h_filename"
+
+# configure for each arch
+export CC=gcc-$gccver
+export CXX=g++-$gccver
+export LD=g++-$gccver
+for arch in "${arches[@]}"; do
+  export CFLAGS="-arch $arch -isysroot $sdk -mmacosx-version-min=$minver"
+  export CXXFLAGS="$CFLAGS"
+  export LDFLAGS="$CFLAGS"
+  export OBJCFLAGS="$CFLAGS"
+  
+  # all set up, now run
+  pushd $tempdir
+  ./configure $extra_config_opts
+  popd
+  
+  # add this config.h to the master copy
+  cat >> $outconfig <<EOM
+#ifdef __${arch}__
+EOM
+  cat $inconfig >> $outconfig
+  cat >> $outconfig <<EOM
+#endif
+EOM
+
+  # add sndfile.h to the master copy
+  cat >> $configdir/sndfile.h <<EOM
+#ifdef __${arch}__
+EOM
+  cat $tempdir/src/sndfile.h >> $configdir/sndfile.h
+  cat >> $configdir/sndfile.h <<EOM
+#endif
+EOM
+
+done
+
+# remove source tree copy
+rm -rf $tempdir
