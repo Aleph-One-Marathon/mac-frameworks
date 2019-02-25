3102cb98597a42a7d2fc0eb9a98ad44db543488d
Hopper262 <hopper@whpress.com>
2019-02-25 18:00:28 -0500

    Support additional compile tools

--- vpx/build.sh
+++ vpx/build.sh
@@ -5,6 +5,7 @@ export VERSION="1.8.0"
 export URL="https://github.com/webmproject/libvpx/archive/v1.8.0.tar.gz"
 export DIRNAME="lib$PROJ-$VERSION"
 export CONFIGOPTS="--disable-examples --disable-unit-tests --target=x86_64-darwin13-gcc"
+export PATH_EXTRA="/usr/local/bin" # for nasm
 export NOPACKAGING="1"
 
 ../build-std.sh
